15980878e221fbf5abd010de97a34874ebcd16c4
Fabián Kozynski <kozynski@google.com>
2025-09-09 14:28:55 -0400

    Add support for internet -> wifi migration
    This adds support for clicking on wifi tile as well as for getting the
    correct tile defaults.
    
    Test: atest PlatformScenarionTests
    Bug: 201143076
    Flag: com.android.systemui.qs_split_internet_tile
    Change-Id: Id64a8994f3268946e5896b792f61619ac3202290

--- libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/quicksettings/QSEditTileSpec.kt
+++ libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/quicksettings/QSEditTileSpec.kt
@@ -24,6 +24,7 @@ package android.platform.systemui_tapl.ui.quicksettings
  */
 enum class QSEditTileSpec(val desc: String, val spec: String) {
     INTERNET("Internet", "internet"),
+    WIFI("Internet", "wifi"),
     BLUETOOTH("Bluetooth", "bt"),
     MODES("Modes", "dnd"),
     AUTO_ROTATE("Auto-rotate", "rotation"),
