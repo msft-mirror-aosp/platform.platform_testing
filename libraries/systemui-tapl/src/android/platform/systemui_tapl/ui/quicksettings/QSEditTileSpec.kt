f77f95a1acbf59196a72d3cae605c326fd052e00
Fabián Kozynski <kozynski@google.com>
2025-10-27 15:24:28 -0400

    Change wifi tile name
    Test: atest PlatformScenarioTests
    Bug: 201143076
    Flag: com.android.systemui.qs_split_internet_tile
    Change-Id: I80d80e2a13ed81eea80d5b5d93333b5ef45b50e2

--- libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/quicksettings/QSEditTileSpec.kt
+++ libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/quicksettings/QSEditTileSpec.kt
@@ -26,7 +26,7 @@ package android.platform.systemui_tapl.ui.quicksettings
  */
 enum class QSEditTileSpec(val desc: String, val spec: String) {
     INTERNET("Internet", "internet"),
-    WIFI("Internet", "wifi"),
+    WIFI("Wi-Fi", "wifi"),
     BLUETOOTH("Bluetooth", "bt"),
     MODES("Modes", "dnd"),
     AUTO_ROTATE("Auto-rotate", "rotation"),
