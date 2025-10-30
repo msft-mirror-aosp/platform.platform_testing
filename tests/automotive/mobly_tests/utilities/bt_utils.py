44de3cf99d7d8028e88b64fb1c1f6b102194c14b
Megha Shetty <shettymegha@google.com>
2025-10-30 09:16:53 +0000

    Adding time after the pairing to allow Assistant pop up
    Test: https://android-build.corp.google.com/test_investigate/invocation/I10600010469690217/test/TR43029962260897633/
    Bug: 457652087
    Bug: 457650920
    Bug: 457651150
    
    Change-Id: Ief76437ac47ba05f9ae720923fc08bab8bb2347f

--- tests/automotive/mobly_tests/utilities/bt_utils.py
+++ tests/automotive/mobly_tests/utilities/bt_utils.py
@@ -128,7 +128,7 @@ class BTUtils:
 
     def allow_permissions_after_pairing(self):
         self.press_allow_on_phone()
-        time.sleep(constants.DEFAULT_WAIT_TIME_FIVE_SECS)
+        time.sleep(constants.DEFAULT_WAIT_TIME_FIFTEEN_SECS)
         self.handle_assistant_pop_up()
 
     def unpair(self):
