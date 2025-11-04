f64481485d964a4f3d3b500155cdaeceacb76f64
Priya Tandon <tandonp@google.com>
2025-11-04 11:45:39 -0800

    Bluetooth HardKeys test Test: https://android-build.corp.google.com/builds/abtd/run/L08200030057655676 Bug: 469430157
    Change-Id: I89bb44f9943e6babf5652ec7a5b974222f67a63b

--- tests/automotive/mobly_tests/bluetooth_hardkeys/bluetooth_hardkeys_answer_call_test.py
+++ tests/automotive/mobly_tests/bluetooth_hardkeys/bluetooth_hardkeys_answer_call_test.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2023 The Android Open Source Project
+#  Copyright (C) 2025 The Android Open Source Project
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,14 +12,14 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Test of basic calling with given digits
+"""Test of bluetooth hard keys calling
  Steps include:
         1) Precall state check on IVI device and phone devices. (OK)
-        3) Make a call to any digits number using IVI device
-        4) Assert calling number on IVI device same as ten digits number
+        3) make a call using hard keys
+        4) Assert the calling number
         5) End call on IVI device
         6) Get latest dialed number from the IVI device
-        7) Assert dialed number on the IVI device same as called ten digits number
+        7) Assert dialed number on the IVI device same as dialed number
 """
 
 from bluetooth_test import bluetooth_base_test
@@ -28,33 +28,37 @@ from utilities import constants
 from utilities.main_utils import common_main
 
 
-class BluetoothDialTest(bluetooth_base_test.BluetoothBaseTest):
+class BluetoothHardKeysCallingTest(bluetooth_base_test.BluetoothBaseTest):
+
+    def setup_class(self):
+        super().setup_class()
+        ro_product_name = str(self.target.adb.shell(constants.GET_PRODUCT_NAME))
+        self.use_dialer_simulator = constants.CF_X86_64_PHONE in ro_product_name
 
     def setup_test(self):
         # Pair the devices
         self.bt_utils.pair_primary_to_secondary()
         super().enable_recording()
 
-    def test_dial_large_digits_number(self):
-        #Variable
-        dialer_test_phone_number = constants.INFORMATION_THREE_DIGIT_NUMBER
+    def test_bluetooth_hard_keys_answer_end_call(self):
+        dialer_test_phone_number = "900900900"
+
+         # call from the unpaired phone to the paired phone
+        if self.use_dialer_simulator:
+          # simulate incoming call
+          self.target.adb.shell(constants.DIALER_SIMULATOR_INCOMING_CALL_COMMAND.format(phone_number="dialer_test_phone_number", name="Jane Doe"))
+
         #Tests the calling three digits number functionality
-        logging.info(
-            'Calling from %s calling to %s',
-            self.target.serial,dialer_test_phone_number
-        )
+        logging.info('Calling from %s calling to %s',self.target.serial,dialer_test_phone_number)
+
         self.call_utils.wait_with_log(2)
-        self.call_utils.dial_a_number(dialer_test_phone_number)
-        self.call_utils.make_call()
-        self.call_utils.wait_with_log(5)
-        self.call_utils.verify_dialing_number(dialer_test_phone_number)
+        self.discoverer.mbs.pressReceiveCallKey()
         self.call_utils.wait_with_log(2)
         self.call_utils.is_ongoing_call_displayed_on_home(True)
-        self.call_utils.open_phone_app_from_home()
-        self.call_utils.end_call()
-        self.call_utils.wait_with_log(5)
-        self.call_utils.open_call_history()
-        self.call_utils.verify_last_dialed_number(dialer_test_phone_number)
+        self.call_utils.wait_with_log(2)
+        self.discoverer.mbs.pressEndCallKey()
+        self.call_utils.is_ongoing_call_displayed_on_home(False)
+
 
     def teardown_test(self):
         # End call if test failed
