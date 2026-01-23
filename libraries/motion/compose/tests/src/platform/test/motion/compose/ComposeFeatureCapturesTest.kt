9f14a4549112c1f10178de1e8e484e94575a9c66
Mike Schneider <michschn@google.com>
2026-01-23 01:28:44 -0800

    Revert "Migrate Compose Motion Tests to use v2 ComposeTestRule"
    This reverts commit bfffbf108a2d6b69a6a966a22af35d58949b1281.
    
    Reason for revert: b/478112067
    
    Bug: 477544904
    Change-Id: I25c477f08d8ba5a0a84a4677314b56db0cefb86f

--- libraries/motion/compose/tests/src/platform/test/motion/compose/ComposeFeatureCapturesTest.kt
+++ libraries/motion/compose/tests/src/platform/test/motion/compose/ComposeFeatureCapturesTest.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.hasTestTag
-import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntSize
