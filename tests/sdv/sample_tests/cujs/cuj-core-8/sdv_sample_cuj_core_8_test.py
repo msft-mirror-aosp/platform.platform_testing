82534e8a9a79feaeff92d85b79616959298d3b9e
Raphael Blistein <raphy@google.com>
2026-02-10 09:30:27 +0000

    Rename APEX com.sdv.google.sample.bar / baz / qux
    Bug: 479193519 479193436 479195478
    Test: m
    Change-Id: I7e704c7689b01a9bba4a4d8f36f16d7ba3d92032

--- tests/sdv/sample_tests/cujs/cuj-core-8/sdv_sample_cuj_core_8_test.py
+++ tests/sdv/sample_tests/cujs/cuj-core-8/sdv_sample_cuj_core_8_test.py
@@ -32,11 +32,11 @@ from sdv_test_fw.verification import polling
 
 class SdvCujCore8Test(sdv_base_test.SdvBaseTestClass, parameterized.TestCase):
 
-    SAMPLES_LOGCAT_ARGS_ALL_BUNDLES = '*:F com_android_sdv_sample_foo_ServiceBundleFoo_instance:* com_sdv_google_sample_bar_ServiceBundleBar_instance:* com_sdv_google_sample_baz_ServiceBundleBaz_instance:*'
+    SAMPLES_LOGCAT_ARGS_ALL_BUNDLES = '*:F com_android_sdv_sample_foo_ServiceBundleFoo_instance:* com_android_sdv_sample_bar_ServiceBundleBar_instance:* com_android_sdv_sample_baz_ServiceBundleBaz_instance:*'
     SAMPLES_LOGCAT_ARGS_LIFECYCLE_MANAGER = '*:F lifecycle_manager:*'
     SAMPLES_LOGCAT_ARGS_FOO = '*:F com_android_sdv_sample_foo_ServiceBundleFoo_instance:*'
-    SAMPLES_LOGCAT_ARGS_BAR = '*:F com_sdv_google_sample_bar_ServiceBundleBar_instance:*'
-    SAMPLES_LOGCAT_ARGS_BAZ = '*:F com_sdv_google_sample_baz_ServiceBundleBaz_instance:*'
+    SAMPLES_LOGCAT_ARGS_BAR = '*:F com_android_sdv_sample_bar_ServiceBundleBar_instance:*'
+    SAMPLES_LOGCAT_ARGS_BAZ = '*:F com_android_sdv_sample_baz_ServiceBundleBaz_instance:*'
 
     # Test Parameters - logcat messages.
     STARTING_TEXT = 'Starting {vm_instance}:{package}.{bundle_name}/instance'
@@ -119,14 +119,14 @@ class SdvCujCore8Test(sdv_base_test.SdvBaseTestClass, parameterized.TestCase):
             'testcase_name': 'baz',
             'device_name': 'foo_baz_device',
             'vm_instance': 'instance1',
-            'package_name': 'com.sdv.google.sample.baz',
+            'package_name': 'com.android.sdv.sample.baz',
             'bundle_name' : 'ServiceBundleBaz'
         },
         {
             'testcase_name': 'bar',
             'device_name': 'bar_device',
             'vm_instance': 'instance2',
-            'package_name': 'com.sdv.google.sample.bar',
+            'package_name': 'com.android.sdv.sample.bar',
             'bundle_name' : 'ServiceBundleBar'
         },
     )
