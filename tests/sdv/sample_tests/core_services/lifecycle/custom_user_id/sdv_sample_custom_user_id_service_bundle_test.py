835bed0da2998c485ef8181794d16baa35157587
Xin Ye <xinner@google.com>
2026-02-27 07:08:32 -0800

    SDV: Refactor logcat polling in sdv_sample_custom_user_id_service_bundle_test.
    Replaced custom logcat polling and assertion logic with the `polling.wait_and_verify_expected_logs` utility. Updated test start/end logging to use `self.log_test_info`.
    
    Tested:atest SdvSampleCustomUserIdTest
    Bug:452852070
    Change-Id: Ic14dbe697a2c39d3525c155aea534ec30c7abe6c

--- tests/sdv/sample_tests/core_services/lifecycle/custom_user_id/sdv_sample_custom_user_id_service_bundle_test.py
+++ tests/sdv/sample_tests/core_services/lifecycle/custom_user_id/sdv_sample_custom_user_id_service_bundle_test.py
@@ -16,10 +16,8 @@
 
 Tests runs on single SDV VM.
 """
-from mobly import asserts
-import logging
-import time
 from sdv_test_fw.test_execution import sdv_base_test, sdv_test_runner
+from sdv_test_fw.verification import polling
 
 
 class SdvSampleCustomUserIdTest(
@@ -30,70 +28,30 @@ class SdvSampleCustomUserIdTest(
 
     SERVICE_BUNDLE_FQIN = "sdv_service_bundle start local-vm:com.sdv.google.sample.lifecycle.userid.apex.LifecycleCustomUserIdSampleServiceBundle/instance-1"
 
-    SAMPLES_LOGCAT_ARGS = "oem_service_bundle_custom_userid_sample::service:"
+    SAMPLES_LOGCAT_ARGS = "oem_service_bundle_custom_userid_sample:*"
 
     def create_service_bundle(self):
         return self.sdv_device.adb().execute_shell_command(self.SERVICE_BUNDLE_FQIN)
 
-    def wait_for_logcat(self, sdv_device, grep_text, timeout=30, poll_interval=0.1):
-        """Polls the logcat output for a specific text until found or timeout.
-
-        Args:
-            grep_text: The text to search for in the logcat output.
-            timeout: The maximum time (in seconds) to wait.
-            poll_interval: The time (in seconds) between polls.
-        Returns:
-            True if grep matched at least one logcat output
-            False if grep matched no logcat output within the timeout
-        """
-        deadline = time.perf_counter() + timeout
-        while time.perf_counter() < deadline:
-            logcat_grep_result = sdv_device.adb().grep_from_logcat(
-                grep = grep_text)
-            if logcat_grep_result != "":
-                return True
-            time.sleep(poll_interval)
-        return False
-
-    def assert_logcat(self, grep_text, assert_msg):
-        """Assert-like validator to grep logcat with the `grep_text`.
-
-        Args:
-            grep_text: The text to search for in the logcat output.
-            assert_msg: The failure assertion message.
-        """
-        if not self.wait_for_logcat(self.sdv_device, grep_text):
-            logcat_grep_result = self.sdv_device.adb().grep_from_logcat(grep_text, self.SAMPLES_LOGCAT_ARGS)
-            asserts.assert_in(
-                grep_text,
-                logcat_grep_result,
-                assert_msg + "\n {logcat_grep_result}"
-            )
-
     def verify_user_id(self):
-        self.assert_logcat(grep_text = self.ASSERT_MESSAGE, assert_msg = self.ERROR_MESSAGE)
-
-
-    def log_enter(self):
-        """ Unified logging enter test suit. """
-        logging.info(f"{self.get_suite_name()} :: Start test {self.current_test_info.name}")
-
-    def log_exit(self):
-        """ Unified logging exit test suit. """
-        logging.info(f"{self.get_suite_name()} :: Finished test {self.current_test_info.name}")
+        polling.wait_and_verify_expected_logs(
+            sdv_device=self.sdv_device.adb(),
+            grep_text=self.ASSERT_MESSAGE,
+            logcat_args=self.SAMPLES_LOGCAT_ARGS,
+            assert_msg=self.ERROR_MESSAGE,
+        )
 
     def setup_class(self):
         super().setup_class()
         self.sdv_device = self.get_device("device1")
 
     def test_custom_user_id(self):
-        self.log_enter()
+        self.log_test_info("Started")
 
         self.create_service_bundle()
         self.verify_user_id()
 
-        self.log_exit()
-
+        self.log_test_info("Finished")
 
 if __name__ == "__main__":
     # Start Test Execution Using SDV Test Framework
