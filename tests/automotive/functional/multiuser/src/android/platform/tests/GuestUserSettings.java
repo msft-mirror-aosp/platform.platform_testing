fb1b219ccd2e49562d5394445a86c724717da69d
Megha Shetty <shettymegha@google.com>
2026-03-17 09:12:49 +0000

    Remove Maps from Dock verification Test: https://android-build.corp.google.com/abtd/run/L34200030089780341/ https://android-build.corp.google.com/abtd/run/L02600030089779639/ Bug: 490563637
    Change-Id: Ifadae844eacda38db2cb0d7790a5430aed562aea

--- tests/automotive/functional/multiuser/src/android/platform/tests/GuestUserSettings.java
+++ tests/automotive/functional/multiuser/src/android/platform/tests/GuestUserSettings.java
@@ -98,11 +98,6 @@ public class GuestUserSettings {
 
     @Test
     public void testDockAndAllAppsOnGuest() {
-        Log.i(LOG_TAG, "Assert: Google Maps App is Present on DOCK");
-        assertTrue(
-                "Google Maps App is NOT Present on Dock",
-                mAppGridHelper.get().verifyAppOnDock(AutomotiveConfigConstants.MAPS_APP_ON_DOCK));
-
         Log.i(LOG_TAG, "Assert: Play Store App is Present on DOCK");
         assertTrue(
                 "Playstore App is NOT Present on Dock",
